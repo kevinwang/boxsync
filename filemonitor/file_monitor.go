b1845311eb33f9167f0ff88452bc800bf08563bd
Ben Ni <amingni2@illinois.edu>
2016-11-26 14:49:19 -0600

    public methods done in file_monitor.go, not tested

--- filemonitor/file_monitor.go
+++ filemonitor/file_monitor.go
@@ -38,28 +38,66 @@ type FileWatchEvent struct {
 //Public methods start here
 
 func NewWatcher(callback onFileEventCallback) *FileWatcher {
-	//Todo, implement this method
-	return
+	fileWatcher := new(FileWatcher)
+
+	var err error
+	fileWatcher.watcher, err = fsnotify.NewWatcher()
+
+	//fsnotify fails...
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		fileWatcher = nil
+	} else {
+		fileWatcher.triggerInstsMap = map[string]*triggerInst{}
+		fileWatcher.callback = callback
+		//start a thread to watch
+		go fileWatcher.startRunning()
+	}
 }
 
 func (fileWatcher *FileWatcher) Watch(filepath string) {
-	//Todo, implement this method
-	return
+	//Has not been initlized, do nothing now.
+	if fileWatcher.watcher == nil {
+		return
+	}
+
+	fileWatcher.mutexLock.Lock()
+	defer fileWatcher.mutexLock.Unlock()
+
+	err := fileWatcher.watcher.Watch(filepath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "tried to watch, but err:", err)
+	}
 }
 
-func (fileWatch *FileWatcher) RemoveWatch(filepath string) {
-	//Todo, implement this method
-	return
+func (fileWatcher *FileWatcher) RemoveWatch(filepath string) {
+	//Has not been initilized, do nothing now.
+	if fileWatcher.watcher == nil {
+		return
+	}
+
+	fileWatcher.mutexLock.Lock()
+	def fileWatcher.mutexLock.Unlock()
+
+	err := fileWatcher.watcher.RemoveWatch(filepath);
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
 }
 
 func (fileWatcher *FileWatcher) WaitForKill() {
-	//Todo, implement this method
-	return
+	onkillChannel := make(chan os.Signal, 1)
+
+	//just try to capture kill, and interrupt signals now...
+	signal.Notify(onkillChannel, os.Interrupt, os.Kill)
+	<-onkillChannel
+	fmt.Fprintln(os.Stderr, "\n kill signal triggerred, exiting...")
 }
 
 func (fileWatcher *FileWatcher) Close() {
-	//Todo, implement this method
-	return
+	//do clean up here
+	fileWatcher.watcher.Close()
+	fileWatcher.watcher = nil
 }
 
 //Private methods start here
