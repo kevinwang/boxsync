12c19210a475ebff9a1641c672844c65da22a9a5
Lu Huang <luhuang3@illinois.edu>
2017-02-13 17:24:23 -0600

    add folder and file structs. The GetFolderEntity is not working.(SegFault)

--- box/file.go
+++ box/file.go
@@ -1,1 +1,18 @@
 package box
+
+import (
+	"encoding/json"
+)
+
+func (c *client) GetFile(fileID string) (*File, error) {
+	body, err := c.Get("/files/" + fileID)
+	if err != nil {
+		return nil, err
+	}
+	var file File
+	err = json.Unmarshal(body, &file)
+	if err != nil {
+		return nil, err
+	}
+	return &file, nil
+}
--- box/folder.go
+++ box/folder.go
@@ -1,1 +1,58 @@
 package box
+
+import (
+	"encoding/json"
+)
+
+func (c *client) GetFolder(folderId string) (*Folder, error) {
+	req, err := c.Get("/folders/" + folderId)
+	if err != nil {
+		return nil, err
+	}
+	var folder Folder
+	err = json.Unmarshal(req, &folder)
+	if err != nil {
+		return nil, err
+	}
+	return &folder, nil
+}
+
+func (c *client) GetAllItems(folderId string) ([]Entity, error) {
+	req, err := c.Get("/folders/" + folderId + "/items")
+	if err != nil {
+		return nil, err
+	}
+
+	var res Collection
+	err = json.Unmarshal(req, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Entry, nil
+}
+
+func (c *client) GetFolderEntity(folderId string) (*FolderEntity, error) {
+	items, err := c.GetAllItems(folderId)
+	if err == nil {
+		return nil, nil
+	}
+	//var folderEntity FolderEntity
+	//fmt.Println("%6.2f", 12.0)
+	var files []File
+	var folders []Folder
+	for _, ety := range items {
+		if ety.IsFile() {
+			var file File
+			ety.toFile(&file)
+			files = append(files, file)
+		}
+		if ety.IsFolder() {
+			var folder Folder
+			ety.toFolder(&folder)
+			folders = append(folders, folder)
+		}
+	}
+
+	return &FolderEntity{FolderId: folderId, Files: files, Folders: folders}, nil
+}
